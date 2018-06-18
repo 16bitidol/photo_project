80630c6c25f4b535a831c59ec58c46439cc3d550
16bitidol <hanihoni07@gmail.com>
2018-06-18 15:28:50 +0000

    imageuploaderまわりしゅうせい

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,20 @@
+class ImageFileTypeValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    if value.present?
+      file_type = File::extname(value.to_s)
+      file_types = %w(.jpg .jpeg .png .gif)
+      record.errors[attribute] << I18n.t('errors.messages.invalid_image') unless file_types.include?(file_type)
+    end
+  end
+end
+
 class Photo < ApplicationRecord
-  mount_uploader :picture, PictureUploader
+  mount_uploader :picture, PictureUploader, mount_on: :file_name
+  
+  before_save :update_asset_attributes
   
   validates :title, presence: true
   validates :title, length: { in: 1..30 } 
-  validates :picture, presence: true
+
+  validates :picture, presence: true, image_file_type: true
 end
