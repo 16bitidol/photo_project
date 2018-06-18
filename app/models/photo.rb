18db499da52716209b69292517f7ba14149f44f8
16bitidol <hanihoni07@gmail.com>
2018-06-18 16:46:54 +0000

    hoge er

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,20 +1,9 @@
-class ImageFileTypeValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    if value.present?
-      file_type = File::extname(value.to_s)
-      file_types = %w(.jpg .jpeg .png .gif)
-      record.errors[attribute] << I18n.t('errors.messages.invalid_image') unless file_types.include?(file_type)
-    end
-  end
-end
-
 class Photo < ApplicationRecord
-  mount_uploader :picture, PictureUploader, mount_on: :file_name
-  
-  before_save :update_asset_attributes
+  mount_uploader :picture, PictureUploader
+
   
   validates :title, presence: true
   validates :title, length: { in: 1..30 } 
 
-  validates :picture, presence: true, image_file_type: true
+  validates :picture, presence: true
 end
